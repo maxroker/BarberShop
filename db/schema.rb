9261861bd6af74bc143533ce0bb8f971fe99c31e
DankoMaksym <dankomaksym@gmail.com>
2019-10-25 23:00:57 +0300

    update index view

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_25_155619) do
+ActiveRecord::Schema.define(version: 2019_10_25_161656) do
 
-  create_table "barber", force: :cascade do |t|
+  create_table "barbers", force: :cascade do |t|
     t.text "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
